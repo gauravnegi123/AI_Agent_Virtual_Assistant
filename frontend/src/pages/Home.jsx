ffdbb7a440b4c2e26d6945517c9be46ae91a6e30
gauravnegi123 <gauravnegi6783@gmail.com>
2026-08-14 15:24:21 +0530

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -203,7 +203,7 @@ function Home() {
       }
     };
 
-    const greeting = new SpeechSynthesisUtterance(`Hello ${userData.name}, bosdeke ?`);
+    const greeting = new SpeechSynthesisUtterance(`Hello ${userData.name}, bhosdike ?`);
     greeting.lang ='en-US';
     window.speechSynthesis.speak(greeting);
 
